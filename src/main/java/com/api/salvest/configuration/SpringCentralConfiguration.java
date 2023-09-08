ba561ee937cb25c2ea047f4e1879283a1da9fb62
thefreakingmind <salmansiddiqui100@gmail.com>
2023-09-08 11:29:13 +0530

    Fixed Java 8 Build

--- src/main/java/com/api/salvest/configuration/SpringCentralConfiguration.java
+++ src/main/java/com/api/salvest/configuration/SpringCentralConfiguration.java
@@ -1,5 +1,6 @@
 package com.api.salvest.configuration;
 
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
